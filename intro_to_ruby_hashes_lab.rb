b46c882ca5409534b2dcd2e23ecf31ff2dc399a4
Jamal Gelin <gelin.jamal@gmail.com>
2018-03-18 23:44:38 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,13 +9,16 @@ end
 def monopoly
 	monopoly = {
 		:railroads =>
-				{
+		{
 					:pieces => 4,
-					:rent_in_dollars:
+					:rent_in_dollars =>
+								{
 
-				}
+								}
 
-	}
+		}
+
+						}
 
 end
 
