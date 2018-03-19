f8fd32c054ca666c7539544818e29c3145ddcfe1
Jamal Gelin <gelin.jamal@gmail.com>
2018-03-19 00:00:30 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly
 					:names =>
 									{:reading_railroad => {"mortgage_value" => "$100"},
 									 :pennsylvania_railroad => {"mortgage_value" => ""}
-									 :b_and_o_railroad => {}
+									 :b_and_o_railroad => {"mortgage_value" => ""}
 									 :shortline =>	{} }
 
 		}
